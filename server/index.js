f15e219b839b28532b3fc3dffeec17e5f92b5ad5
Prateek Rath <prateekrath28@gmail.com>
2022-11-30 19:44:31 +0530

    update

--- server/index.js
+++ server/index.js
@@ -4,29 +4,29 @@ const fs = require("fs");
 const http = require('http');
 const cors = require('cors')
 const { Server } = require('socket.io')
-const path=require("path")
+const path = require("path")
+
 
 
 const server = http.createServer(app);
 
-const {AffindaCredential, AffindaAPI} = require("@affinda/affinda");
+const { AffindaCredential, AffindaAPI } = require("@affinda/affinda");
 
 const credential = new AffindaCredential("bc10dc6ac0a51291eacbbb05c4da73d05eec7ece")
 const client = new AffindaAPI(credential)
+// const readStream = fs.createReadStream((__dirname + "/Sai-Soumyak-Nanda-2024-2.pdf").toString());
+const readStream=fs.readFileSync("/Sai-Soumyak-Nanda-2024-2.pdf")
 
-const dirPath = path.join(__dirname, "/Sai-Soumyak-Nanda-2024-2.pdf")
-// fs.mkdirSync(dirPath)
 
-
-const readStream = fs.createReadStream(dirPath);
-
-client.createInvoice({file: readStream}).then((result) => {
+client.createResume({file: readStream}).then((result) => {
     console.log("Returned data:");
     console.dir(result)
 }).catch((err) => {
     console.log("An error occurred:");
     console.error(err);
 });
+
+
 
 
 
