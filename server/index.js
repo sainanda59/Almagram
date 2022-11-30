8b3e77cd4ab35a2e0ba9349212ab0f6355289fda
Prateek Rath <prateekrath28@gmail.com>
2022-11-30 15:12:52 +0530

    folder structure

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const fs = require("fs");
 const http = require('http');
 const cors = require('cors')
-const {Server}=require('socket.io')
+const { Server } = require('socket.io')
+const path=require("path")
+
 
 const server = http.createServer(app);
 
@@ -12,8 +14,20 @@ const {AffindaCredential, AffindaAPI} = require("@affinda/affinda");
 const credential = new AffindaCredential("bc10dc6ac0a51291eacbbb05c4da73d05eec7ece")
 const client = new AffindaAPI(credential)
 
-const readStream = fs.createReadStream("server/Sai-Soumyak-Nanda-2024-2.pdf");
+const dirPath = path.join(__dirname, "/Sai-Soumyak-Nanda-2024-2.pdf")
+// fs.mkdirSync(dirPath)
+
+const readStream = fs.createReadStream(dirPath);
 let data;
+console.log(__dirname)
+
+client.createInvoice({file: readStream}).then((result) => {
+    console.log("Returned data:");
+    console.dir(result)
+}).catch((err) => {
+    console.log("An error occurred:");
+    console.error(err);
+});
 
 // client.createResume({file: readStream}).then((result) => {
 //     console.log("Returned data:");
@@ -25,21 +39,22 @@ let data;
 // });
 
 
-client.createInvoice({url: "https://api.affinda.com/static/sample_invoices/"}).then((result) => {
-    console.log("Returned data:");
-    console.dir(result)
-}).catch((err) => {
-    console.log("An error occurred:");
-    console.error(err);
-});
+// client.createInvoice({url: "https://api.affinda.com/static/server/Sai-Soumyak-Nanda-2024-2.pdf"}).then((result) => {
+//     console.log("Returned data:");
+//     console.dir(result)
+//     data = result;
+// }).catch((err) => {
+//     console.log("An error occurred:");
+//     console.error(err);
+// });
 
-client.getResume(data).then((result) => {
-    console.log("Returned data:");
-    console.dir(result)
-}).catch((err) => {
-    console.log("An error occurred:");
-    console.error(err);
-});
+// client.getResume(data).then((result) => {
+//     console.log("Returned data:");
+//     console.dir(result)
+// }).catch((err) => {
+//     console.log("An error occurred:");
+//     console.error(err);
+// });
 
 
 
